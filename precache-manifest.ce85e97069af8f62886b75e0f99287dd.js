525dbed8b11d4f014a3270690164209f1b0472c8
segundara <segundara@gmail.com>
2020-03-07 23:00:43 +0200

    Updates

---
+++ precache-manifest.ce85e97069af8f62886b75e0f99287dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566ca6177861f44d9c482f874c2ed92b",
+    "url": "/face_detection_app/index.html"
+  },
+  {
+    "revision": "22b9239a6701c76e0ca0",
+    "url": "/face_detection_app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0b8dd1b0a6cc930cda65",
+    "url": "/face_detection_app/static/css/main.6cab6b9a.chunk.css"
+  },
+  {
+    "revision": "22b9239a6701c76e0ca0",
+    "url": "/face_detection_app/static/js/2.fbde94df.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/face_detection_app/static/js/2.fbde94df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8dd1b0a6cc930cda65",
+    "url": "/face_detection_app/static/js/main.84777a8f.chunk.js"
+  },
+  {
+    "revision": "b57d5fc109348d493511",
+    "url": "/face_detection_app/static/js/runtime-main.78c37474.js"
+  }
+]);
