764de67a53cfb41dff72fe845e5e7a93fc7ec16c
segundara <segundara@gmail.com>
2020-03-09 23:27:26 +0200

    Form text update

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 	return (
 		<div>
 			<p className='f3'>
-				{'This magic brain will detect faces in your pictures'}
+				{'This smart app will detect faces in your pictures'}
 			</p>
 			<div className='center'>
 				<div className='center form pa4 br3 shadow-5'>
